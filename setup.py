0de2324adf5358650cdcbeea929997e1619a57d2
Charles Rymal <charles.rymal@nortek.com>
2017-06-22 17:07:21 -0700

    use pkgtool

--- setup.py
+++ setup.py
@@ -1,26 +1,10 @@
-import re
+import os
 from setuptools import setup
 
-with open('ws_sheets/__init__.py') as f:
-    version = re.findall("^__version__ = '(.*)'", f.read())[0]
+import pkgtool
 
-with open('requirements.txt') as f:
-    install_requires=[l.strip() for l in f.readlines()]
+pkg = pkgtool.Package(os.getcwd())
 
-setup(name='ws_sheets',
-        version=version,
-        description='python spreadsheets',
-        url='http://github.com/chuck1/ws_sheets',
-        author='Charles Rymal',
-        author_email='charlesrymal@gmail.com',
-        license='MIT',
-        packages=[
-            'ws_sheets',
-            'ws_sheets.tests',
-            'ws_sheets.tests.conf',
-            'ws_sheets.ext.middleware',
-            ],
-        install_requires=install_requires,
-        zip_safe=False,
-        )
+setup(**pkg.setup_args())
+
 
